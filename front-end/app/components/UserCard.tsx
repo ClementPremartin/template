209570a13c0e404353b53b169dd967f908b0a504
Clement <clementpremartin@gmail.com>
2024-02-26 15:48:19 +0100

    modify user componants, fetching graphql API & setup User style with tailwind

---
+++ front-end/app/components/UserCard.tsx
@@ -0,0 +1,36 @@
+import { User } from '../users/page'
+import Image from 'next/image'
+
+type PropsType = {
+  user: User
+}
+
+const UserCard = ({ user }: PropsType) => {
+  return (
+    <div className="flex min-w-0 gap-x-4 w-full ml-5 mr-5">
+      <Image
+        className="h-12 w-12 flex-none rounded-full bg-gray-50"
+        src={`https://gravatar.com/avatar/${user.id}?s=400&d=robohash&r=x`}
+        alt="Profile picture"
+        width={100}
+        height={100}
+      />
+      <div className="min-w-0 flex-auto">
+        <p className="text-sm font-semibold leading-6 text-gray-900">
+          {user.firstname} {user.lastname}
+        </p>
+        <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+          {user.email}
+        </p>
+      </div>
+      <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+        <p className="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
+        <p className="mt-1 text-xs leading-5 text-gray-500">
+          Last seen <time dateTime="2023-01-23T13:23Z">3h ago</time>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default UserCard
--- front-end/app/users/page.tsx
+++ front-end/app/users/page.tsx
@@ -1,12 +1,83 @@
-import ProductCard from '../components/ProductCard'
+'use client'
 
-const usersPage = () => {
+import UserCard from '../components/UserCard'
+import { useEffect, useState } from 'react'
+
+const USERS = `
+  query getUsers {
+    getUsers {
+      email
+      firstname
+      id
+      lastname
+    }
+  }
+`
+export type User = {
+  email: string
+  firstname: string
+  id: number
+  lastname: string
+}
+
+const UsersPage = () => {
+  const [users, setUsers] = useState<User[] | null | undefined>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [isError, setIsError] = useState<boolean>(false)
+  const [error, setError] = useState<String>('')
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: USERS }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok')
+        }
+        return res.json()
+      })
+      .then((res: any) => {
+        const users: User[] = res.data.getUsers
+        if (users && users !== null) {
+          setUsers(users)
+        }
+        setIsLoading(false)
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        setIsError(true)
+        setError(`Error fetching data: ${error}`)
+      })
+  }, [])
+  if (isLoading)
+    return (
+      <>
+        <div className="animate-pulse bg-red-600 w-52 flex flex-col rounded-md p-3">
+          <h1 className="flex justify-center">User Card</h1>
+        </div>
+      </>
+    )
+
+  if (isError) return <p>Error : {error}</p>
   return (
     <>
-      <div>usersPage</div>
-      <ProductCard />
+      <h1>Users List</h1>
+      <ul role="list" className="divide-y divide-gray-100">
+        {users &&
+          users?.map((user) => {
+            return (
+              <li className="flex justify-between gap-x-6 py-5" key={user.id}>
+                <UserCard user={user} />
+              </li>
+            )
+          })}
+      </ul>
     </>
   )
 }
 
-export default usersPage
+export default UsersPage
